fdf7d2ba15713ae3cd388a0b011503b01b87ba1a
AndreTerronQuiroz <143724321+AndreTerronQuiroz@users.noreply.github.com>
2023-09-07 08:46:47 -0600

    Add files via upload

---
+++ areaRectangulo.java
@@ -0,0 +1,21 @@
+
+public class areaRectangulo {
+
+    
+    public static void main(String[] args) 
+    {
+        int base = 10;
+        int altura = 20;
+        
+        int areaRectangulo = base*altura;
+        
+        double baseDecimal = 8.6;
+        double alturaDecimal = 14.7;
+        
+        double areaRectanguloDecimal = baseDecimal*alturaDecimal;
+        
+        System.out.println("mi nombre es Andre Terron Quiroz y el area de un rectangulo con enteros es: " + areaRectangulo);
+        System.out.println("y el area de un rectangulo con decimales es: " + areaRectanguloDecimal);
+    }
+    
+}
