2ab1e97a7ca4e299dfb377c2dfd6074ce25753d0
AndreTerronQuiroz <143724321+AndreTerronQuiroz@users.noreply.github.com>
2023-09-12 07:49:38 -0600

    Add files via upload

---
+++ Listadetareas.java
@@ -0,0 +1,26 @@
+
+package listadetareas;
+
+
+public class Listadetareas {
+
+    
+    public static void main(String[] args) {
+        
+        int tarea;
+        String tarea1 = "Investigar";
+        String tarea2 = "Resolver ejercicios";
+        String tarea3 = "realizar expocicion";
+        String tarea4 = "estudiar para el examen";
+        String tarea5 = "realizar un programa en java";
+        
+        System.out.println("Primera tarea; " 
+                + tarea1 + "segunda tarea;" 
+                + tarea2 + " tercer tarea;" 
+                + tarea3 + "cuarta tarea;"
+                + tarea4 +"quinta tarea;"
+                + tarea5);
+
+    }
+    
+}
